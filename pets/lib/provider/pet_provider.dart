307212aefb959657ca04f71cd12fba273c8bc004
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-06 22:45:10 -0300

    Missed dep

--- pets/lib/provider/pet_provider.dart
+++ pets/lib/provider/pet_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pets/models/pet.dart';
 import 'package:pets/provider/http_provider.dart';
