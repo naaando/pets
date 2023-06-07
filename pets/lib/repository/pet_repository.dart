96875965610447afd8021181fe48f2c56ea5fad6
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-06 23:35:53 -0300

    Remove dependency

--- pets/lib/repository/pet_repository.dart
+++ pets/lib/repository/pet_repository.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart';
 import 'package:pets/models/pet.dart';
 
