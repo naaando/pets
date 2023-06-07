b82a3d9234c3190f961cafea62cc83d73963258f
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-06 23:09:30 -0300

    Add specie providers

---
+++ pets/lib/provider/specie_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pets/models/specie.dart';
+import 'package:pets/provider/http_provider.dart';
+import 'package:pets/provider/user_provider.dart';
+import 'package:pets/repository/specie_repository.dart';
+
+final specieRepositoryProvider =
+    ChangeNotifierProvider<SpecieRepository>((ref) {
+  final httpClient = ref.watch(httpClientProvider);
+  final token = ref.watch(tokenProvider);
+
+  return SpecieRepository(httpClient, token);
+});
+
+final petsProvider = FutureProvider<Map<String, Specie>>(
+    (ref) => ref.watch(specieRepositoryProvider).findAll());
+
+final petProvider = FutureProvider.family<Specie?, String>(
+    (ref, id) async => ref.watch(specieRepositoryProvider).find(id));
