db8ccc19803add6b8091610515145625f15a49df
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-08 23:37:44 -0300

    Configure intl date formatting

--- pets/lib/main.dart
+++ pets/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:pets/app.dart';
 
 void main() {
+  initializeDateFormatting();
+
   runApp(
     const ProviderScope(
       child: App(),
