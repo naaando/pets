34611d80555d807e5c80e04c0058c30de3488d46
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-08 23:45:45 -0300

    Add jiffy

--- pets/lib/main.dart
+++ pets/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:jiffy/jiffy.dart';
 import 'package:pets/app.dart';
 
 void main() {
-  initializeDateFormatting();
+  initializeDateFormatting('pt_BR');
+  Jiffy.setLocale('pt_BR');
 
   runApp(
     const ProviderScope(
