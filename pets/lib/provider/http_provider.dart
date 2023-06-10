b181abce3f9fb3d9eee91a0b1c93849085f16a5f
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-10 13:07:45 -0300

    Move dependencies into HttpClient

--- pets/lib/provider/http_provider.dart
+++ pets/lib/provider/http_provider.dart
@@ -3,6 +3,5 @@ import 'package:http/http.dart' as http;
 import 'package:pets/http_client.dart';
 
 final httpClientProvider = Provider((ref) {
-  const token = 'AfXaHy0-1h1ELUsRZUafMsOFPBXPb9HF';
-  return HttpClient(http.Client(), token);
+  return HttpClient();
 });
