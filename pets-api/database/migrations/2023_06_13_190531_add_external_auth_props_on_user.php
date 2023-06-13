a947006ddc574bd080bc5a60b967f6568c3f353c
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-13 16:09:42 -0300

    Migration to use social login

---
+++ pets-api/database/migrations/2023_06_13_190531_add_external_auth_props_on_user.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('iss')->nullable();
+            $table->string('sub')->nullable();
+            $table->string('picture')->nullable();
+            $table->string('locale')->nullable();
+            $table->string('password')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['iss', 'sub', 'picture', 'locale']);
+            $table->string('password')->change();
+        });
+    }
+};
