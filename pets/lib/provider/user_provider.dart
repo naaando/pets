78ccbd673fb3e36cd2d961c2e02f6edcba703326
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-14 21:04:51 -0300

    Atualiza provider de usuarios

--- pets/lib/provider/user_provider.dart
+++ pets/lib/provider/user_provider.dart
@@ -1,4 +1,11 @@
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pets/models/user.dart';
+import 'package:pets/repository/user_repository.dart';
 
-final userProvider = StateProvider<GoogleSignInAccount?>((ref) => null);
+final userRepositoryProvider = ChangeNotifierProvider<UserRepository>((ref) {
+  return UserRepository();
+});
+
+final userProvider =
+    FutureProvider<User?>((ref) => ref.watch(userRepositoryProvider).getUser());
