22f66bda56ed7a47b6506121ed2d2f6776e08dba
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-15 20:07:27 -0300

    Adc isar provider

---
+++ pets/lib/provider/isar_provider.dart
@@ -0,0 +1,15 @@
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pets/models/espaco.dart';
+import 'package:pets/models/user.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+@riverpod
+Future<Isar> isar() async {
+  final dir = await getApplicationDocumentsDirectory();
+
+  return await Isar.open(
+    [UserSchema, EspacoSchema],
+    directory: dir.path,
+  );
+}
