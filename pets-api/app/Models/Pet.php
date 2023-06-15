31ed319a4b2d2381e42a79d3318ef0070896a4c0
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-15 12:45:20 -0300

    Usa uuid em vez de ulid

--- pets-api/app/Models/Pet.php
+++ pets-api/app/Models/Pet.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pet extends Model
 {
     use HasFactory;
-    use HasUlids;
+    use HasUuids;
 
     /**
      * The attributes that are mass assignable.
