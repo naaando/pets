f1f78307ec47a379f013fb08596bec30a001dcda
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-17 16:34:39 -0300

    Remove deps não usadas

--- pets/lib/repository/pet_repository.dart
+++ pets/lib/repository/pet_repository.dart
@@ -1,9 +1,7 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 import 'package:http_parser/http_parser.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:mime/mime.dart';
-import 'package:pets/models/espaco.dart';
 import 'package:pets/models/pet.dart';
 import 'package:pets/http_client.dart';
 
