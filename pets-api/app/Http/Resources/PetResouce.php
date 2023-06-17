2c9c90012a4e530ee2d25a01d6eaccc0032435fb
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-17 20:47:17 -0300

    Adiciona resources

--- pets-api/app/Http/Resources/PetResouce.php
+++ pets-api/app/Http/Resources/PetResouce.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserResource extends JsonResource
+class PetResouce extends JsonResource
 {
     /**
      * Transform the resource into an array.
