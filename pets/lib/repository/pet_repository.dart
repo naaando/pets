df7ef4cc1bae429eee4fe0c9a06915ba7a9da7d1
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-17 16:34:17 -0300

    Remove ações com estado no PetRepository

--- pets/lib/repository/pet_repository.dart
+++ pets/lib/repository/pet_repository.dart
@@ -7,41 +7,23 @@ import 'package:pets/models/espaco.dart';
 import 'package:pets/models/pet.dart';
 import 'package:pets/http_client.dart';
 
-class PetRepository extends ChangeNotifier {
-  Map<String, Pet> pets = <String, Pet>{};
+class PetRepository {
   final HttpClient httpClient;
 
   PetRepository(this.httpClient);
 
   Future<Pet?> find(String id, {bool forceRefresh = false}) async {
-    if (pets[id] != null && !forceRefresh) {
-      return pets[id];
-    }
-
     var jsonPet = await httpClient.getJson('api/pets/$id');
-    var pet = Pet.fromJson(jsonPet);
-    pets[pet.id!] = pet;
-
-    notifyListeners();
-
-    return pets[id];
+    return Pet.fromJson(jsonPet);
   }
 
   Future<Map<String, Pet>> findAll(
       {String? espacoId, bool forceRefresh = false}) async {
-    if (pets.isNotEmpty && !forceRefresh) {
-      return Future.value(pets);
-    }
-
     var jsonPets = await httpClient.getJson('api/pets?espaco_id=$espacoId');
 
     // ignore: prefer_for_elements_to_map_fromiterable
-    pets = Map<String, Pet>.fromIterable(jsonPets,
+    return Map<String, Pet>.fromIterable(jsonPets,
         key: (json) => json['id'], value: (json) => Pet.fromJson(json));
-
-    notifyListeners();
-
-    return pets;
   }
 
   Future save(Pet pet) async {
@@ -53,17 +35,11 @@ class PetRepository extends ChangeNotifier {
       savedPet = await _update(pet);
     }
 
-    pets[savedPet.id!] = savedPet;
-
-    notifyListeners();
+    return savedPet;
   }
 
   remove(Pet pet) async {
     await httpClient.delete('api/pets/${pet.id}');
-
-    pets.remove(pet.id);
-
-    notifyListeners();
   }
 
   Future<Pet> _create(Pet pet) async {
