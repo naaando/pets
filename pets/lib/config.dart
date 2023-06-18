9d5c6945118871e60c73509ea351eb0779c3152f
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-17 21:45:11 -0300

    Remove config de servidor antiga

--- pets/lib/config.dart
+++ pets/lib/config.dart
@@ -1,6 +1,1 @@
-const String token = 'AfXaHy0-1h1ELUsRZUafMsOFPBXPb9HF';
-
 final Uri baseUri = Uri.parse('http://10.0.2.2:9999/');
-
-final Uri baseUriWithAccessToken =
-    baseUri.replace(queryParameters: {'access_token': token});
