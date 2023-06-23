667ff85848c928571df6e447cca51add8691a3d3
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-23 11:47:52 -0300

    Adiciona factories

---
+++ pets-api/database/factories/PetFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Espaco;
+use App\Models\Especie;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\=Pet>
+ */
+class PetFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nome' => $this->faker->name,
+            'imagem' => $this->faker->imageUrl(),
+            'user_id' => User::factory(),
+            'espaco_id' => Espaco::factory(),
+            'especie_id' => Especie::factory(),
+            'sexo' => $this->faker->randomElement(['macho', 'femea']),
+            'raca' => $this->faker->randomElement(['SRD', 'Poodle', 'Vira-lata']),
+            'nascimento' => $this->faker->dateTimeBetween('-10 years', '-1 year'),
+            'falecimento' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'castracao' => $this->faker->dateTimeBetween('-1 year', 'now'),
+        ];
+    }
+}
