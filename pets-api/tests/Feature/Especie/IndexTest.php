ef2fd6d05372f3d8b3694df0f968c7850c1cff47
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-23 12:29:20 -0300

    Fix tests

--- pets-api/tests/Feature/Especie/IndexTest.php
+++ pets-api/tests/Feature/Especie/IndexTest.php
@@ -1,8 +1,10 @@
 <?php
 
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\getJson;
 
 it('can list all especies', function () {
+    actingAs($user = \App\Models\User::factory()->create());
     $response = getJson('api/especies');
     $response->assertStatus(200);
 });
--- pets-api/tests/Feature/Pets/IndexTest.php
+++ pets-api/tests/Feature/Pets/IndexTest.php
@@ -1,8 +1,11 @@
-<?
+<?php
 
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\getJson;
 
 test('empty list', function () {
+    actingAs($user = \App\Models\User::factory()->create());
+
     $response = getJson('/api/pets');
     $response->assertStatus(200);
     $response->isEmpty();
