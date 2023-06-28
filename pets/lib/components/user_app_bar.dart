1c8f667e2ce395419db8dd81b01d4a3c3bba9539
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-28 19:55:24 -0300

    Usa appbar padrão

--- pets/lib/components/user_app_bar.dart
+++ pets/lib/components/user_app_bar.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:pets/components/main_tab_bar.dart';
-import 'package:pets/models/user.dart';
 import 'package:pets/provider/user_provider.dart';
 
 class UserAppBar extends HookConsumerWidget implements PreferredSizeWidget {
@@ -10,59 +8,36 @@ class UserAppBar extends HookConsumerWidget implements PreferredSizeWidget {
   });
 
   @override
-  Size get preferredSize => const Size.fromHeight(60);
+  Size get preferredSize => AppBar().preferredSize;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     var user = ref.watch(loggedUserProvider).asData!.value!;
 
-    // return AppBar(
-    //   backgroundColor: Colors.transparent,
-    //   shadowColor: Colors.transparent,
-    //   title: Text(
-    //     'Olá, ${user.name}',
-    //     style: const TextStyle(color: Colors.black54),
-    //   ),
-    //   centerTitle: true,
-    //   actions: <Widget>[
-    //     IconButton(
-    //       icon: CircleAvatar(
-    //         radius: 20,
-    //         backgroundImage: NetworkImage(user.picture!),
-    //         backgroundColor: Colors.transparent,
-    //       ),
-    //       onPressed: () {
-    //         // do something
-    //       },
-    //     )
-    //   ],
-    // );
-    return Column(
-      children: [
-        SizedBox(
-          height: 40,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Text(
-              'Olá, ${user.name}',
-              style: const TextStyle(color: Colors.black54),
-            ),
-            const SizedBox(
-              width: 18,
-            ),
-            CircleAvatar(
-              radius: 20,
-              backgroundImage: NetworkImage(user.picture!),
-              backgroundColor: Colors.transparent,
-            ),
-            const SizedBox(
-              width: 18,
-            ),
-          ],
-        )
-      ],
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      shadowColor: Colors.transparent,
+      surfaceTintColor: Colors.transparent,
+      foregroundColor: Colors.transparent,
+      scrolledUnderElevation: 0,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            'Olá, ${user.name}',
+            style: const TextStyle(color: Colors.black54, fontSize: 12),
+          ),
+          const SizedBox(
+            width: 12,
+          ),
+          CircleAvatar(
+            radius: 14,
+            backgroundImage: NetworkImage(user.picture!),
+            backgroundColor: Colors.transparent,
+          )
+        ],
+      ),
     );
   }
 }
