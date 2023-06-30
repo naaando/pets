d163ba4eb787c53d93c384294b291b7bdc3f9def
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-06-29 23:40:08 -0300

    Integração inicial do login do facebook

--- pets/lib/login.dart
+++ pets/lib/login.dart
@@ -3,11 +3,12 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pets/config.dart';
 import 'package:pets/provider/user_provider.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 googleSignIn(WidgetRef ref) async {
   GoogleSignIn googleSignIn = GoogleSignIn(
-    clientId:
-        '323626532097-mo9lg5m12rojsb1h6n3c940sjoifu93u.apps.googleusercontent.com',
+    serverClientId:
+        '323626532097-tnpc95mi6bvjrtkk3sfhpcmkkc1kjlmo.apps.googleusercontent.com',
     scopes: [
       'openid',
       'https://www.googleapis.com/auth/userinfo.email',
@@ -29,4 +30,18 @@ googleSignIn(WidgetRef ref) async {
 
   final userRepository = await ref.read(userRepositoryProvider.future);
   userRepository.setUserWithSanctumToken(sanctumToken.data);
+}
+
+facebookSignIn(WidgetRef ref) async {
+  final facebook = FacebookAuth.instance;
+  final result = await facebook.login();
+
+  Dio dio = Dio();
+  dio.options.headers['access_token'] = result.accessToken;
+
+  // var sanctumToken =
+  await dio.getUri(baseUri.replace(path: '/api/auth/facebook'));
+
+  // final userRepository = await ref.read(userRepositoryProvider.future);
+  // userRepository.setUserWithSanctumToken(sanctumToken.data);
 }
--- pets/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pets/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,17 @@
 import FlutterMacOS
 import Foundation
 
+import facebook_auth_desktop
 import file_selector_macos
+import flutter_secure_storage_macos
 import isar_flutter_libs
 import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
