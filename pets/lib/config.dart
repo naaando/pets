860fe5613b3ccddd58e0b98a6d541757c6fd108c
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-07-06 12:35:13 -0300

    Adiciona config alternativa para facilitar troca de url

--- pets/lib/config.dart
+++ pets/lib/config.dart
@@ -1,1 +1,2 @@
-final Uri baseUri = Uri.parse('https://pets.fssoftware.com.br/');
+// final Uri baseUri = Uri.parse('https://pets.fssoftware.com.br/');
+final Uri baseUri = Uri.parse('http://door1.fssoftware.com.br:21999/');
