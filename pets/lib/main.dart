e294527bc398ab862710d5d696c3401a6df7fbfb
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-07-13 22:18:45 -0300

    Configura timezone

--- pets/lib/main.dart
+++ pets/lib/main.dart
@@ -1,18 +1,33 @@
+import 'dart:io';
+
 import 'package:fast_cached_network_image/fast_cached_network_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:jiffy/jiffy.dart';
 import 'package:pets/app.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+import 'package:flutter_timezone/flutter_timezone.dart';
 
 import 'notifications.dart';
 
+Future<void> _configureLocalTimeZone() async {
+  if (kIsWeb || Platform.isLinux) {
+    return;
+  }
+  tz.initializeTimeZones();
+  final String timeZoneName = await FlutterTimezone.getLocalTimezone();
+  tz.setLocalLocation(tz.getLocation(timeZoneName));
+}
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   initializeDateFormatting('pt_BR');
   Jiffy.setLocale('pt_BR');
-
-  WidgetsFlutterBinding.ensureInitialized();
+  await _configureLocalTimeZone();
 
   await notificationsInitializer();
 
