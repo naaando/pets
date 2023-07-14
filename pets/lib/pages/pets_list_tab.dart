bf2ae3cd2b840202dac5740d29f6ff5c5ce17b5e
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-07-14 01:44:28 -0300

    Usa fuzzy search para busca rankeada

--- pets/lib/pages/pets_list_tab.dart
+++ pets/lib/pages/pets_list_tab.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:jiffy/jiffy.dart';
 import 'package:pets/components/fab_actions.dart';
 import 'package:pets/models/pet.dart';
 import 'package:pets/provider/pet_provider.dart';
+import 'package:fuzzywuzzy/fuzzywuzzy.dart';
 
 class PetsListTab extends HookConsumerWidget {
   const PetsListTab({super.key});
@@ -33,7 +35,11 @@ class PetsListTab extends HookConsumerWidget {
   Widget body(context, Iterable<Pet> pets) {
     ValueNotifier<String> pesquisa = useState('');
 
-    var petsFiltrados = pets.where((pet) => pet.nome.contains(pesquisa.value));
+    var petsFiltrados = pets.toList()
+      ..sort((a, b) {
+        return ratio(b.nome, pesquisa.value)
+            .compareTo(ratio(a.nome, pesquisa.value));
+      });
 
     return SingleChildScrollView(
       padding: const EdgeInsets.all(15),
@@ -67,33 +73,134 @@ class PetsListTab extends HookConsumerWidget {
   }
 
   Widget petCard(BuildContext context, Pet pet) {
+    textOrPlaceholder(value, placeholder) => TextSpan(
+          text: value ?? placeholder,
+          style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                color: value != null ? Colors.grey[900] : Colors.grey[600],
+              ),
+        );
+
     return InkWell(
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 15),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            leadingImage(context, pet),
-            Padding(
-                padding: const EdgeInsets.only(left: 22, top: 6),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      pet.nome,
-                      style: Theme.of(context).textTheme.titleLarge,
+          padding: const EdgeInsets.symmetric(vertical: 15),
+          child: SizedBox(
+            height: 120,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                leadingImage(context, pet),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.topRight,
+                    padding: const EdgeInsets.only(left: 22, top: 6),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Text(
+                          pet.nome,
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // idade
+                                RichText(
+                                    text: TextSpan(children: [
+                                  TextSpan(
+                                    text: 'Idade ',
+                                    style:
+                                        Theme.of(context).textTheme.bodySmall,
+                                  ),
+                                  textOrPlaceholder(
+                                      pet.nascimento != null
+                                          ? Jiffy.parse(pet.nascimento ?? '')
+                                              .fromNow()
+                                          : null,
+                                      'indefinido'),
+                                ])),
+                                RichText(
+                                    text: TextSpan(children: [
+                                  TextSpan(
+                                    text: 'Espécie ',
+                                    style:
+                                        Theme.of(context).textTheme.bodySmall,
+                                  ),
+                                  textOrPlaceholder(
+                                      pet.especie?.nome, 'indefinida'),
+                                ])),
+                                const SizedBox(height: 3),
+                                RichText(
+                                    text: TextSpan(children: [
+                                  TextSpan(
+                                    text: 'Sexo ',
+                                    style:
+                                        Theme.of(context).textTheme.bodySmall,
+                                  ),
+                                  textOrPlaceholder(pet.sexo, 'indefinido'),
+                                ])),
+                                const SizedBox(height: 3),
+                                RichText(
+                                    text: TextSpan(children: [
+                                  TextSpan(
+                                    text: 'Raça ',
+                                    style:
+                                        Theme.of(context).textTheme.bodySmall,
+                                  ),
+                                  textOrPlaceholder(pet.raca, 'indefinida'),
+                                ])),
+                              ],
+                            ),
+                            const SizedBox(width: 20),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Badge(
+                                  backgroundColor: pet.castrado == true
+                                      ? Colors.teal
+                                      : Colors.orange,
+                                  label: pet.castrado == true
+                                      ? const Text('Castrado')
+                                      : const Text('Não castrado'),
+                                ),
+                                const SizedBox(height: 3),
+                                Badge(
+                                  backgroundColor: pet.sexo == 'macho'
+                                      ? Colors.blue
+                                      : Colors.pink[300],
+                                  label: pet.sexo == 'macho'
+                                      ? const Text('Macho')
+                                      : const Text('Fêmea'),
+                                ),
+                                const SizedBox(height: 3),
+                                Badge(
+                                  backgroundColor: pet.obito == null
+                                      ? Colors.lightGreen
+                                      : Colors.grey,
+                                  label: pet.obito == null
+                                      ? const Text('Vivo')
+                                      : const Text('Falecido'),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
                     ),
-                    Text(
-                      pet.especieId ?? '',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                  ],
-                )),
-          ],
-        ),
-      ),
+                  ),
+                ),
+              ],
+            ),
+          )),
       onTap: () {
         Navigator.pushNamed(context, '/cadastro-pet', arguments: pet);
       },
