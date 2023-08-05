40d73550b1d11f6d560f39ebcdea5f360aa69669
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-08-05 18:10:14 -0300

    Adiciona softDelete em Pets

--- pets-api/app/Models/Pet.php
+++ pets-api/app/Models/Pet.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * App\Models\Pet
@@ -53,6 +54,7 @@ class Pet extends Model
 {
     use HasFactory;
     use HasUuids;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
