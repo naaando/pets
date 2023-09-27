1a3f81bb5de7e0ea30ad8a0326b1c2fc27ebadd9
Fernando da Silva Sousa <wild.nando@gmail.com>
2023-09-27 15:30:36 -0300

    Desabilita seção de Repetir

--- pets/lib/pages/medicacao_page.dart
+++ pets/lib/pages/medicacao_page.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
